2ffc1c841396a9aafc717faf26d694981143c46a
Andree Requejo <andree09requejo@gmail.com>
2025-10-07 00:29:56 -0500

    fix: actualizar parámetros en el archivo de configuración

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import torchvision
 from torchvision.models import EfficientNet_V2_M_Weights
 
-# PATHS AND FILES
+# RUTAS Y ARCHIVOS
 TRAIN_FILES = [
     "ACNE04/NNEW_trainval_0.txt", #0
     "ACNE04/NNEW_trainval_1.txt", #1
@@ -31,19 +31,19 @@ TEST_FILES = [
 
 IMAGE_PATH = "ACNE04/JPEGImages"
 
-# TRAINING PARAMETERS
+# PARÁMETROS DE ENTRENAMIENTO
 BATCH_SIZE = 16
-NUM_EPOCHS = 15
+NUM_EPOCHS = 10
 LEARNING_RATE = 0.0001
 VALIDATION_SPLIT = 0.2
 SMOOTHING = 0.12
 
-# IMAGE TRANSFORMS
+# TRANSFORMACIONES DE IMAGEN
 MEAN = (0.5, 0.5, 0.5)
 STD = (0.5, 0.5, 0.5)
 IMAGE_SIZE = (224, 224)
 
-# DATA TRANSFORMS
+# TRANSFORMACIONES DE DATOS
 TRAIN_TRANSFORM = torchvision.transforms.Compose([
     torchvision.transforms.Resize(IMAGE_SIZE),
     torchvision.transforms.RandomHorizontalFlip(p=0.5),
@@ -60,14 +60,13 @@ TEST_TRANSFORM = torchvision.transforms.Compose([
     torchvision.transforms.Normalize(MEAN, STD)
 ])
 
-# MODEL SAVE PATH
+# RUTA DE GUARDADO DEL MODELO
 MODEL_SAVE_PATH = "./acne_model.pt"
 MODEL_PESOS_PATH = "core/best.pt"
 
-# CLASS NAMES (ajusta según tu dataset)
+# NOMBRES DE CLASES (ajusta según tu dataset)
 CLASS_NAMES = [
     "Leve",      # Clase 0
     "Moderado",  # Clase 1
     "Severo",    # Clase 2
-    #"Muy Severo" # Clase 3
 ]
